14faaba07a994f90b80427fba37b45e1075f5633
agsgokul <agsgokul6@gmail.com>
2025-11-09 12:41:19 +0530

    Update API URL for production

--- frontend/src/axiosInstance.jsx
+++ frontend/src/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 let axiosInstance=axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://cable-admin-backend.onrender.com",
 })
 export default axiosInstance;
